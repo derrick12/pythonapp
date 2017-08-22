34234250743f487ecdb4dbff4a71a838c8d97935
derrick12 <derrickwaweru@gmail.com>
2017-08-22 21:25:24 +0300

    add print student function

--- app/functions.py
+++ app/functions.py
@@ -5,3 +5,11 @@ def get_students_titlecase():
     for student in students:
         students_titlecase = student.title()
     return students_titlecase
+def print_students_titlecase():
+      #  students_titlecase = []
+  #  for student in students:                       
+  #      students_titlecase = student.title()
+
+  # Instead of the above repeated function body we use this instead :
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
