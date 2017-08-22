4a6e2ecb3523b78a78e9cdd0490838074059415d
derrick12 <derrickwaweru@gmail.com>
2017-08-22 22:00:31 +0300

    add a new file

---
+++ app/functions2.py
@@ -0,0 +1,8 @@
+students = []
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student["name"].title()
+    return students_titlecase
